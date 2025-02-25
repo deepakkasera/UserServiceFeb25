29c92e84b1dca507874d73683aa9e3c50e12bc49
deepakkasera <deepak.kasera143.dk@gmail.com>
2025-02-25 23:22:11 +0530

    Kafka

--- src/main/java/com/example/userservicefeb25/services/UserServiceImpl.java
+++ src/main/java/com/example/userservicefeb25/services/UserServiceImpl.java
@@ -1,11 +1,14 @@
 package com.example.userservicefeb25.services;
 
-import ch.qos.logback.classic.util.CopyOnInheritThreadLocal;
+import com.example.userservicefeb25.dtos.SendEmailDto;
 import com.example.userservicefeb25.models.Token;
 import com.example.userservicefeb25.models.User;
 import com.example.userservicefeb25.repositories.TokenRepository;
 import com.example.userservicefeb25.repositories.UserRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -13,20 +16,25 @@ import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.Date;
 import java.util.Optional;
-import java.util.UUID;
 
 @Service
 public class UserServiceImpl implements UserService {
     private BCryptPasswordEncoder bCryptPasswordEncoder;
     private UserRepository userRepository;
     private TokenRepository tokenRepository;
+    private KafkaTemplate<String, String> kafkaTemplate; //Key -> Topic, Value -> Event
+    private ObjectMapper objectMapper;
 
     public UserServiceImpl(BCryptPasswordEncoder bCryptPasswordEncoder,
                            UserRepository userRepository,
-                           TokenRepository tokenRepository) {
+                           TokenRepository tokenRepository,
+                           KafkaTemplate kafkaTemplate,
+                           ObjectMapper objectMapper) {
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
         this.userRepository = userRepository;
         this.tokenRepository = tokenRepository;
+        this.kafkaTemplate = kafkaTemplate;
+        this.objectMapper = objectMapper;
     }
 
     @Override
@@ -64,6 +72,22 @@ public class UserServiceImpl implements UserService {
         user.setEmail(email);
         user.setName(name);
         user.setPassword(bCryptPasswordEncoder.encode(password));
+
+        //Push an event to Kafka which UserService will read and send a Welcome email to the user.
+        SendEmailDto emailDto = new SendEmailDto();
+        emailDto.setSubject("Welcome to Scaler!!");
+        emailDto.setBody("Happy to have you onboard.");
+        emailDto.setEmail(email);
+
+        //Push an event to Kafka
+        try {
+            kafkaTemplate.send(
+                    "sendEmail",
+                    objectMapper.writeValueAsString(emailDto)
+            );
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
 
         return userRepository.save(user);
     }
